1f6ec902c47db6d09bb9b8de19efdad1f8d368d3
jakaria07 <jakaria0207@gmail.com>
2025-01-21 15:19:15 +0600

    real time notification

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,34 +1,105 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
+import './Navbar.css'; // Assuming you will create a separate CSS file for styling
 
 function Navbar() {
-  return (
-    
-      <div>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Full Stack Applicatioin
-          </Link>
-    <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
+  const [notifications, setNotifications] = useState([]);
+  const [showTray, setShowTray] = useState(false); // To toggle the notification tray
 
-    <Link className="btn btn-outline-light" to="/adduser">
-        Add User  
-    </Link>
-                  
-  </div>
-</nav>
+  useEffect(() => {
+    // Establish WebSocket connection
+    const socket = new SockJS('http://localhost:8080/ws');
+    const stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+      stompClient.subscribe('/topic/notifications', function (notification) {
+        setNotifications((prevNotifications) => [
+          ...prevNotifications,
+          notification.body,
+        ]);
+      });
+    });
+
+    return () => {
+      if (stompClient) {
+        stompClient.disconnect();
+      }
+    };
+  }, []);
+
+  const toggleTray = () => {
+    setShowTray(!showTray); // Toggle the notification tray
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Full Stack Application
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto"> {/* This pushes items to the right */}
+              <li className="nav-item">
+                {/* Add User button */}
+                <Link className="btn btn-outline-light" to="/adduser">
+                  Add User
+                </Link>
+              </li>
+
+              <li className="nav-item dropdown">
+                {/* Notification button */}
+                <button
+                  className="btn btn-light mx-2 position-relative"
+                  onClick={toggleTray}
+                >
+                  <i className="fas fa-bell"></i>
+                  <span className="badge bg-danger notification-count">
+                    {notifications.length}
+                  </span>
+                </button>
+
+                {/* Notification Tray */}
+                {showTray && (
+                  <div className="notification-tray">
+                    <h5 className="tray-header">Notifications</h5>
+                    <ul className="notification-list">
+                      {notifications.length > 0 ? (
+                        notifications.map((notification, index) => (
+                          <li key={index} className="notification-item">
+                            {notification}
+                          </li>
+                        ))
+                      ) : (
+                        <li className="notification-item">
+                          No new notifications
+                        </li>
+                      )}
+                    </ul>
+                  </div>
+                )}
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,79 +1,96 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
-
     const [users, setUsers] = useState([]);
-
     const { id } = useParams();
 
-
     useEffect(() => {
-        console.log("Code with Jakaria");
         loadUsers();
-    },[]);
+
+        // WebSocket setup using SockJS and STOMP
+        const socket = new SockJS('http://localhost:8080/ws');  // Use SockJS with correct URL
+        const stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, (frame) => {
+            stompClient.subscribe('/topic/notifications', (message) => {
+                toast.success(message.body);  // Show notification via toast
+            });
+        });
+
+        return () => {
+            if (stompClient) {
+                stompClient.disconnect();
+            }
+        };
+    }, []);
 
     const loadUsers = async () => {
-        const result = await axios.get("http://localhost:8080/users");
-        setUsers(result.data);
+        try {
+            const result = await axios.get("http://localhost:8080/users");
+            setUsers(result.data);
+        } catch (error) {
+            toast.error("Error loading users");
+        }
     };
 
     const deleteUser = async (id) => {
-        if (!id) {
-            console.log("No Id Found!!");
+        try {
+            await axios.delete(`http://localhost:8080/user/${id}`);
+            toast.success(`User with ID ${id} deleted successfully!`);
+            loadUsers();
+        } catch (error) {
+            toast.error("Error deleting user");
         }
-        await axios.delete(`http://localhost:8080/user/${id}`);
-        loadUsers();
     }
 
-  return (
-    <div className='container'>
-        <div className='py-5'>
-            <table className="table border shadow">
-                <thead>
-                    <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Action</th>    
-                    </tr>
-                </thead>
-                <tbody>
-                      
-                      {
-                          users.map((user, index) => (
-                            <tr>
-                                <th scope="row" key={index}>{index + 1}</th>
-                                <td>{user.name}</td>
-                                <td>{user.username}</td>
-                                <td>{user.email}</td>
-                                <td>
-                                      <Link className="btn btn-primary mx-2"
-                                          to={`/viewuser/${user.id}`}
-                                      >
-                                          View</Link>
-                                      <Link className="btn btn-outline-primary mx-2"
-                                          
-                                          to={`/edituser/${user.id}`}
-                                      >
-                                          Edit
-                                      </Link>
-                                      <button className="btn btn-danger mx-2"
-                                          onClick={()=>deleteUser(user.id)}
-                                      >
-                                          Delete
-                                      </button>
-                                      
-                                </td>  
-                            </tr>
-                          ))
-                      }
-                    
-                </tbody>
-            </table>
-         </div>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <ToastContainer />  {/* This will display the notifications */}
+            <div className='py-5'>
+                <table className="table border shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>    
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => (
+                                <tr key={index}>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <Link className="btn btn-primary mx-2"
+                                            to={`/viewuser/${user.id}`}>
+                                            View
+                                        </Link>
+                                        <Link className="btn btn-outline-primary mx-2"
+                                            to={`/edituser/${user.id}`}>
+                                            Edit
+                                        </Link>
+                                        <button className="btn btn-danger mx-2"
+                                            onClick={() => deleteUser(user.id)}>
+                                            Delete
+                                        </button>
+                                    </td>  
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
